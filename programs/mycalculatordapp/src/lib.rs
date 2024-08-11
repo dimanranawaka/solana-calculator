67bf89295a0d7fb158d9b3a8379b63d12bf12fa6
dimanranawaka <dimanranawaka@gmail.com>
2024-08-11 23:38:14 +0530

    implement create function of calc

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -6,7 +6,7 @@ declare_id!("HjpXgiRpxr3iJP9AeRxr6xzyqMMEUH5BzbfkUr8j1mTX");
 pub mod mycalculatordapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Create>,init_message:String) -> Result<()> {
+    pub fn create(ctx: Context<Create>,init_message:String) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.greeting = init_message;
         Ok(());
